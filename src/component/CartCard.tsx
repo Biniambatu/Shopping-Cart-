4e448f37fe38ae5fae3dfcc6ce0966796db56dd2
Biniam batu <Biniambatu12@gmail.com>
2024-05-29 22:49:10 +0300

    useReducer

--- src/component/CartCard.tsx
+++ src/component/CartCard.tsx
@@ -1,14 +1,16 @@
 
 import "../component/CartCard.css"
+import { useCart } from "../context/CartContext";
 
 export const CartCard = ({product}) => {
   const {name, price, image} = product;
+  const {removeFromCart} = useCart()
   return (
     <div className='cartCard'>
      <img src={image} alt=''/>
      <p className='productName'>{name}</p>
      <p className='productPrice'>${price}</p>
-     <button>Remove</button>
+     <button onClick={()=>removeFromCart(product)}>Remove</button>
     </div>
   )
 }
