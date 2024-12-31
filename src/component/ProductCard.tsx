cb369cd1a1d62cf640e4f207f3cb14c4d0c71e0e
Biniam batu <Biniambatu12@gmail.com>
2024-12-31 18:37:55 +0300

    c

--- src/component/ProductCard.tsx
+++ src/component/ProductCard.tsx
@@ -4,7 +4,7 @@ import { useCart } from '../context/CartContext';
 import './ProductCard.css'
 export const ProductCard = ({product}) => {
    const {addToCart,removeFromCart,cartList} = useCart()
-   const {id,name, price, image} = product;
+   const {id,name, price, image, auther} = product;
    const [isInCart, setIsInCart] = useState(false)
    useEffect(()=>{
       const productIsInCart = cartList.find((find) => find.id === product.id)
@@ -19,12 +19,11 @@ export const ProductCard = ({product}) => {
     <div className="productCard">
        <img src={image} alt=""/>
        <p className="name">{name}</p>
+       <p>{auther?.name}</p>
        <div className="action">
           <p>${price}</p>
           {isInCart ? (<button className='remove' onClick={()=>removeFromCart(product)}>Remove</button>) : (<button onClick={()=>addToCart(product)}>Add To Cart</button>)}
-          
        </div>
-       
     </div>
   )
 }
